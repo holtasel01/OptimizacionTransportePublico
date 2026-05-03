2ff499f3aa9cbc6e9a5cf9aa2921b42df15ed02a
Uziel Mejia <uzielkaleb@hotmail.com>
2026-05-02 23:58:49 -0600

    Cambio de arquitectura e implementacion de estaciones

---
+++ backend/app/knowledge/stations.py
@@ -0,0 +1,389 @@
+stations = {
+    """
+    Glosario de lineas:
+    
+    L1: Linea 1 del tren ligero
+    L2: Linea 2 del tren ligero
+    L3: Linea 3 del tren ligero
+    L4: Linea 4 del tren ligero
+    L5: Macro aeropuerto pero no existe, esta en planes
+    L6: Macro Calzada 
+    L7: Macro Periferico
+    """
+
+    
+    "Periferico Norte": {
+        "name": "Periferico Norte",
+        "aliases": {
+            "L1": "Periferico Norte",
+            "L7": "Periferico Norte"
+        },
+        "lines": ["L1", "L7"]
+    },
+    "Dermatologico": {
+        "name": "Dermatologico",
+        "aliases": {
+            "L1": "Dermatologico"
+        },
+        "lines": ["L1"]
+    },
+    "Atemajac": {
+        "name": "Atemajac",
+        "aliases": {
+            "L1": "Atemajac"
+        },
+        "lines": ["L1"]
+    },
+    "Division del Norte": {
+        "name": "Division del Norte",
+        "aliases": {
+            "L1": "Division del Norte"
+        },
+        "lines": ["L1"]
+    },
+    "Avila Camacho": {
+        "name": "Avila Camacho",
+        "aliases": {
+            "L1": "Avila Camacho",
+            "L3": "Avila Camacho"
+        },
+        "lines": ["L1", "L3"]
+    },
+    "Mezquitan": {
+        "name": "Mezquitan",
+        "aliases": {
+            "L1": "Mezquitan"
+        },
+        "lines": ["L1"]
+    },
+    "Refugio": {
+        "name": "Refugio",
+        "aliases": {
+            "L1": "Refugio"
+        },
+        "lines": ["L1"]
+    },
+    "Juarez": {
+        "name": "Juarez",
+        "aliases": {
+            "L1": "Juarez",
+            "L2": "Juarez"
+        },
+        "lines": ["L1", "L2"]
+    },
+    "Mexicaltzingo": {
+        "name": "Mexicaltzingo",
+        "aliases": {
+            "L1": "Mexicaltzingo"
+        },
+        "lines": ["L1"]
+    },
+    "Washington": {
+        "name": "Washington",
+        "aliases": {
+            "L1": "Washington"
+        },
+        "lines": ["L1"]
+    },
+    "Santa Filomena": {
+        "name": "Santa Filomena",
+        "aliases": {
+            "L1": "Santa Filomena"
+        },
+        "lines": ["L1"]
+    },
+    "Unidad Deportiva": {
+        "name": "Unidad Deportiva",
+        "aliases": {
+            "L1": "Unidad Deportiva"
+        },
+        "lines": ["L1"]
+    },
+    "Urdaneta": {
+        "name": "Urdaneta",
+        "aliases": {
+            "L1": "Urdaneta"
+        },
+        "lines": ["L1"]
+    },
+    "18 de Marzo": {
+        "name": "18 de Marzo",
+        "aliases": {
+            "L1": "18 de Marzo"
+        },
+        "lines": ["L1"]
+    },
+    "Isla Raza": {
+        "name": "Isla Raza",
+        "aliases": {
+            "L1": "Isla Raza"
+        },
+        "lines": ["L1"]
+    },
+    "Patria Sur": {
+        "name": "Patria Sur",
+        "aliases": {
+            "L1": "Patria Sur"
+        },
+        "lines": ["L1"]
+    },
+    "España": {
+        "name": "España",
+        "aliases": {
+            "L1": "España"
+        },
+        "lines": ["L1"]
+    },
+    "Santuario Martires de Cristo Rey": {
+        "name": "Santuario Martires de Cristo Rey",
+        "aliases": {
+            "L1": "Santuario Martires de Cristo Rey"
+        },
+        "lines": ["L1"]
+    },
+    "Periferico Sur": {
+        "name": "Periferico Sur",
+        "aliases": {
+            "L1": "Periferico Sur",
+            "L7": "Periferico Sur"
+        },
+        "lines": ["L1, L7"]
+    },
+    "Centro": {
+        "name": "Centro",
+        "aliases": {
+            "L2": "Plaza Universidad",
+            "L3": "Guadalajara Centro"
+        },
+        "lines": ["L2", "L3"]
+    },
+    "San Juan de Dios": {
+        "name": "San Juan de Dios",
+        "aliases": {
+            "L2": "San Juan de Dios",
+            "L6": "San Juan de Dios"
+        },
+        "lines": ["L2", "L6"]
+    },
+    "Belisario Dominguez": {
+        "name": "Belisario Dominguez",
+        "aliases": {
+            "L2": "Belisario Dominguez"
+        },
+        "lines": ["L2"]
+    },
+    "Oblatos": {
+        "name": "Oblatos",
+        "aliases": {
+            "L2": "Oblatos"
+        },
+        "lines": ["L2"]
+    },
+    "Cristobal de Oñate": {
+        "name": "Cristobal de Oñate",
+        "aliases": {
+            "L2": "Cristobal de Oñate"
+        },
+        "lines": ["L2"]
+    },
+    "San Andres": {
+        "name": "San Andres",
+        "aliases": {
+            "L2": "San Andres"
+        },
+        "lines": ["L2"]
+    },
+    "San Jacinto": {
+        "name": "San Jacinto",
+        "aliases": {
+            "L2": "San Jacinto"
+        },
+        "lines": ["L2"]
+    },
+    "La Aurora": {
+        "name": "La Aurora",
+        "aliases": {
+            "L2": "La Aurora"
+        },
+        "lines": ["L2"]
+    },
+    "Tetlan": {
+        "name": "Tetlan",
+        "aliases": {
+            "L2": "Tetlan"
+        },
+        "lines": ["L2"]
+    },
+    "Arcos de Zapopan": {
+        "name": "Arcos de Zapopan",
+        "aliases": {
+            "L3": "Arcos de Zapopan"
+        },
+        "lines": ["L3"]
+    },
+    "Periferico Belenes": {
+        "name": "Periferico Belenes",
+        "aliases": {
+            "L3": "Periferico Belenes",
+            "L7": "Periferico Belenes"
+        },
+        "lines": ["L3", "L7"]
+    },
+    "Mercado del Mar": {
+        "name": "Mercado del Mar",
+        "aliases": {
+            "L3": "Mercado del Mar"
+        },
+        "lines": ["L3"]
+    },
+    "Zapopan Centro": {
+        "name": "Zapopan Centro",
+        "aliases": {
+            "L3": "Zapopan Centro"
+        },
+        "lines": ["L3"]
+    },
+    "Plaza Patria": {
+        "name": "Plaza Patria",
+        "aliases": {
+            "L3": "Plaza Patria"
+        },
+        "lines": ["L3"]
+    },
+    "Circunvalacion Country": {
+        "name": "Circunvalacion Country",
+        "aliases": {
+            "L3": "Circunvalacion Country"
+        },
+        "lines": ["L3"]
+    },
+    "La Normal (CETRAM)": {
+        "name": "La Normal (CETRAM)",
+        "aliases": {
+            "L3": "La Normal (CETRAM)"
+        },
+        "lines": ["L3"]
+    },
+    "Santuario": {
+        "name": "Santuario",
+        "aliases": {
+            "L3": "Santuario"
+        },
+        "lines": ["L3"]
+    },
+    "Independencia": {
+        "name": "Independencia",
+        "aliases": {
+            "L3": "Independencia",
+            "L6": "Bicentenario"
+        },
+        "lines": ["L3", "L6"]
+    },
+    "Plaza de la Bandera": {
+        "name": "Plaza de la Bandera",
+        "aliases": {
+            "L3": "Plaza de la Bandera"
+        },
+        "lines": ["L3"]
+    },
+    "CUCEI": {
+        "name": "CUCEI",
+        "aliases": {
+            "L3": "CUCEI"
+        },
+        "lines": ["L3"]
+    },
+    "Revolucion": {
+        "name": "Revolucion",
+        "aliases": {
+            "L3": "Revolucion"
+        },
+        "lines": ["L3"]
+    },
+    "Rio Nilo": {
+        "name": "Rio Nilo",
+        "aliases": {
+            "L3": "Rio Nilo"
+        },
+        "lines": ["L3"]
+    },
+    "Tlaquepaque Centro": {
+        "name": "Tlaquepaque Centro",
+        "aliases": {
+            "L3": "Tlaquepaque Centro"
+        },
+        "lines": ["L3"]
+    },
+    "Lazaro Cardenas": {
+        "name": "Lazaro Cardenas",
+        "aliases": {
+            "L3": "Lazaro Cardenas"
+        },
+        "lines": ["L3"]
+    },
+    "Central de Autobuses": {
+        "name": "Central de Autobuses",
+        "aliases": {
+            "L3": "Central de Autobuses"
+        },
+        "lines": ["L3"]
+    },
+    "Tlajomulco Centro": {
+        "name": "Tlajomulco Centro",
+        "aliases": {
+            "L4": "Tlajomulco Centro"
+        },
+        "lines": ["L4"]
+    },
+    "CUTLAJO": {
+        "name": "CUTLAJO",
+        "aliases": {
+            "L4": "CUTLAJO"
+        },
+        "lines": ["L4"]
+    },
+    "Lomas del Sur": {
+        "name": "Lomas del Sur",
+        "aliases": {
+            "L4": "Lomas del Sur"
+        },
+        "lines": ["L4"]
+    },
+    "El Cuervo": {
+        "name": "El Cuervo",
+        "aliases": {
+            "L4": "El Cuervo"
+        },
+        "lines": ["L4"]
+    },
+    "Concepcion del Valle": {
+        "name": "Concepcion del Valle",
+        "aliases": {
+            "L4": "Concepcion del Valle"
+        },
+        "lines": ["L4"]
+    },
+    "Real del Valle": {
+        "name": "Real del Valle",
+        "aliases": {
+            "L4": "Real del Valle"
+        },
+        "lines": ["L4"]
+    },
+    "Jalisco 200 Años": {
+        "name": "Jalisco 200 Años",
+        "aliases": {
+            "L4": "Jalisco 200 Años",
+            "L7": "Jalisco 200 Años"
+        },
+        "lines": ["L4", "L7"]
+    },
+    "Las Juntas": {
+        "name": "Las Juntas",
+        "aliases": {
+            "L4": "Las Juntas"
+        },
+        "lines": ["L4"]
+    },
+    
+}
